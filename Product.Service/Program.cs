2427f81beb8547f57d8443a6e5bbd336a275dea6
Josue Correa <josuejtc@gmail.com>
2021-12-30 19:18:51 -0300

    Serilog config

--- Product.Service/Program.cs
+++ Product.Service/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Serilog;
 
 namespace Product.Service
 {
@@ -21,6 +16,9 @@ namespace Product.Service
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).UseSerilog((ctx, lc) => lc
+                    .WriteTo.Console()
+                    .WriteTo.File(@"C:\Microservice\productservice.txt", rollingInterval: RollingInterval.Day)
+                    .WriteTo.Seq("http://localhost:5341"));
     }
 }
--- Product.Service/Startup.cs
+++ Product.Service/Startup.cs
@@ -2,18 +2,12 @@ using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Product.Application;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Product.Service
 {
@@ -26,7 +20,6 @@ namespace Product.Service
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -39,7 +32,6 @@ namespace Product.Service
             AddMediatr(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
